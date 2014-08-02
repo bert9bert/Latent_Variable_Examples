0a30d28034a6318f094d972e68f19a0f4f76c18a
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-08-01 21:55:17 -0500

    finished EM Gaussian mixture example

--- em_gaussian_mixture.py
+++ em_gaussian_mixture.py
@@ -3,22 +3,24 @@ Gaussian mixture example
 '''
 
 import numpy as np
-from scipy.stats import multivariate_normal
+import math
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from statutils import multivariate_normal
 
 ### set program parameters ###
 N = 100  # number of observations
-TOL = 0.001  # EM iteration stops once improvement in log likelihood is below this
-MAXITER = 10  # EM iteration stops once this many iterations are performed
+MAXITER = 100  # EM iteration stops once this many iterations are performed
 
 ### set true parameters for Gaussian mixture ###
 d = 2  # dimensions of Gaussian
 
 # first Gaussian
-mu_dist0 = np.array([2, 50])
+mu_dist0 = np.array([15, 75])
 sigma_dist0 = np.array([[15, 0], [0, 30]])
 
 # second Gaussian
-mu_dist1 = np.array([5, 90])
+mu_dist1 = np.array([70, 30])
 sigma_dist1 = np.array([[10, 0], [0, 12]])
 
 assert mu_dist0.shape==(d,)
@@ -35,10 +37,10 @@ latent_vec = np.random.binomial(1, tau, N)
 
 x_vec = np.empty((N,d))
 for i in range(N):
-	if latent_vec[i]==0:
-		x_vec[i] = np.random.multivariate_normal(mu_dist0, sigma_dist0)
-	else:
-		x_vec[i] = np.random.multivariate_normal(mu_dist1, sigma_dist1)
+    if latent_vec[i]==0:
+        x_vec[i] = np.random.multivariate_normal(mu_dist0, sigma_dist0)
+    else:
+        x_vec[i] = np.random.multivariate_normal(mu_dist1, sigma_dist1)
 
 
 ### Try to estimate parameters with EM algorithm ###
@@ -53,28 +55,69 @@ loglik = np.empty(MAXITER+1)
 
 ## Set initial guesses ##
 mu_dist0_estvec[0] = np.array([10,10])
-sigma_dist0_estvec[0] = np.array([[10, 0], [0, 10]])
+sigma_dist0_estvec[0] = np.array([[10, 0], [0, 20]])
 mu_dist1_estvec[0] = np.array([10,10])
-sigma_dist1_estvec[0] = np.array([[10, 0], [0, 10]])
+sigma_dist1_estvec[0] = np.array([[10, 0], [0, 30]])
 tau_estvec[0] = 0.50
 
 loglik[0] = np.inf
 
 
 ## perform EM ##
+
 T = np.empty((N,2))
 
 for i in range(1,MAXITER+1):
-	# E Step
-	for obs in range(N):
-		denom = (1-tau_estvec[i-1])*multivariate_normal(x_vec[obs], mu_dist0_estvec[i-1], sigma_dist0_estvec[i-1]) + tau_estvec[i-1]*multivariate_normal(x_vec[obs], mu_dist1_estvec[i-1], sigma_dist1_estvec[i-1])
-		T[obs,0] = (1-tau_estvec[i-1])*multivariate_normal(x_vec[obs], mu_dist0_estvec[i-1], sigma_dist0_estvec[i-1])/denom
-		T[obs,1] = tau_estvec[i-1]*multivariate_normal(x_vec[obs], mu_dist1_estvec[i-1], sigma_dist1_estvec[i-1])/denom
+    # E Step
+    for obs in range(N):
+        denom = (1-tau_estvec[i-1])*multivariate_normal(x_vec[obs], mu_dist0_estvec[i-1], sigma_dist0_estvec[i-1]) + \
+            tau_estvec[i-1]*multivariate_normal(x_vec[obs], mu_dist1_estvec[i-1], sigma_dist1_estvec[i-1])
 
+        T[obs,0] = (1-tau_estvec[i-1])*multivariate_normal(x_vec[obs], mu_dist0_estvec[i-1], sigma_dist0_estvec[i-1])/denom
+        T[obs,1] = tau_estvec[i-1]*multivariate_normal(x_vec[obs], mu_dist1_estvec[i-1], sigma_dist1_estvec[i-1])/denom
 
-	# M Step
+    # M Step
+    tau_estvec[i] = np.sum(T[:,1])/N
 
+    mu_dist0_estvec[i] = T[:,0].dot(x_vec)/np.sum(T[:,0])
+    mu_dist1_estvec[i] = T[:,1].dot(x_vec)/np.sum(T[:,1])
+
+    diffpart0 = x_vec-mu_dist0_estvec[i]
+    diffpart1 = x_vec-mu_dist1_estvec[i]
+
+    sigma_dist0_estvec[i] = np.zeros(sigma_dist0_estvec[i].shape)
+    for k in range(N):    
+        sigma_dist0_estvec[i] = sigma_dist0_estvec[i] + \
+            T[k,0]*np.outer(diffpart0[k], diffpart0[k].T)
+    sigma_dist0_estvec[i] = sigma_dist0_estvec[i]/np.sum(T[:,0])
+
+    sigma_dist1_estvec[i] = np.zeros(sigma_dist1_estvec[i].shape)
+    for k in range(N):    
+        sigma_dist1_estvec[i] = sigma_dist1_estvec[i] + \
+            T[k,1]*np.outer(diffpart1[k], diffpart1[k].T)
+    sigma_dist1_estvec[i] = sigma_dist1_estvec[i]/np.sum(T[:,1])
 
 ## trim storage vectors ##
+numiters = MAXITER  # accounts for index 0
 
 ### Display results ###
+tau_est = tau_estvec[numiters]
+mu_dist0_est = mu_dist0_estvec[numiters,:]
+mu_dist1_est = mu_dist1_estvec[numiters,:]
+sigma_dist0_est = sigma_dist0_estvec[numiters,:,:]
+sigma_dist1_est = sigma_dist1_estvec[numiters,:,:]
+
+plt.scatter(x_vec[:,0], x_vec[:,1])
+
+x = np.arange(0, 100, 0.025)
+y = np.arange(0, 100, 0.025)
+X, Y = np.meshgrid(x, y)
+Z0 = mlab.bivariate_normal(X, Y, sigma_dist0_est[0,0], sigma_dist0_est[1,1], mu_dist0_est[0], mu_dist0_est[1], sigma_dist0_est[1,0])
+Z1 = mlab.bivariate_normal(X, Y, sigma_dist1_est[0,0], sigma_dist1_est[1,1], mu_dist1_est[0], mu_dist1_est[1], sigma_dist1_est[1,0])
+
+plt.contour(X,Y,Z0)
+plt.contour(X,Y,Z1)
+
+plt.title('Expectation-Maximization Algorithm Estimation of Two Gaussians')
+
+plt.show()
---
+++ statutils.py
@@ -0,0 +1,27 @@
+'''
+Statistics utilities module
+'''
+
+import numpy as np
+import math
+
+
+def multivariate_normal(x, mu, sigma):
+    # This function is adapted from
+    # http://stackoverflow.com/questions/11615664/multivariate-normal-density-in-python
+
+    size = len(x)
+    if size == len(mu) and (size, size) == sigma.shape:
+        det = np.linalg.det(sigma)
+        if det == 0:
+            raise NameError("The covariance matrix can't be singular")
+  
+        norm_const = 1.0/ ( math.pow((2*np.pi),float(size)/2) * math.pow(det,1.0/2) )
+        x_mu = np.matrix(x - mu)
+        inv = np.linalg.inv(sigma)
+        result = math.pow(math.e, -0.5 * (x_mu * inv * x_mu.T))
+        return norm_const * result
+    else:
+        raise NameError("The dimensions of the input don't match")
+
+
